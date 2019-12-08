6a1dbd1f5822d09ca5f2023d7632a41d0fc44a12
mjain <mjain@mediaocean.com>
2019-12-08 14:12:02 +0530

    Updates

---
+++ precache-manifest.e6593c64a7c8134910b0e7bfc10b823b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e54ee8c409c7d2f8ed7ae9cff3fb53",
+    "url": "/keboli-assignment/index.html"
+  },
+  {
+    "revision": "44302943ac519a187d9e",
+    "url": "/keboli-assignment/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e888d62b95610eb71e9",
+    "url": "/keboli-assignment/static/js/2.40452d0e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/keboli-assignment/static/js/2.40452d0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44302943ac519a187d9e",
+    "url": "/keboli-assignment/static/js/main.5cec49cf.chunk.js"
+  },
+  {
+    "revision": "7e3427e140f8ee207c9b",
+    "url": "/keboli-assignment/static/js/runtime-main.9b5a94b7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/keboli-assignment/precache-manifest.e6593c64a7c8134910b0e7bfc10b823b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keboli-assignment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
